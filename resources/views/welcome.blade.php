f2ad0d3f3a9a210ba427c767b08193e75bf2cdd3
yegenAnil <ygnanil@gmail.com>
2020-03-27 19:40:31 +0300

    title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>icratakip</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -98,3 +98,5 @@
         </div>
     </body>
 </html>
+
+
